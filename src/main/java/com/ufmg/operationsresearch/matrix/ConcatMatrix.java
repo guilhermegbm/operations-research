5bb39de69085a884fa51deef791bc6f71127838e
guilhermegbm <guilhermebmendonca60@gmail.com>
2021-12-08 16:24:42 -0300

    Added method to Concat Matrices Downwards. Improved Unit Tests for concatRight. Added Unit Tests for concatDown

--- src/main/java/com/ufmg/operationsresearch/matrix/ConcatMatrix.java
+++ src/main/java/com/ufmg/operationsresearch/matrix/ConcatMatrix.java
@@ -6,7 +6,7 @@ public class ConcatMatrix {
 
 	public static Matrix concatRight(Matrix m1, Matrix m2) {
 		if (m1.getLines() != m2.getLines()) {
-			throw new InvalidParameterException("Number of lines on m1 (" + m1.getLines() + ") is different from the number of lines on the other matrix (" + m2.getLines() + ")");
+			throw new InvalidParameterException("Number of lines on m1 (" + m1.getLines() + ") is different from the number of lines on m2 (" + m2.getLines() + ")");
 		}
 
 		Matrix newMatrix = m1.duplicate();
@@ -18,6 +18,26 @@ public class ConcatMatrix {
 		for (int l = 1; l <= newMatrix.getLines(); l++) {
 			for (int m2Col = 1; m2Col <= m2.getColumns(); m2Col++) {
 				newMatrix.setValue(l, originalNumberOfColumns + m2Col, m2.getValue(l, m2Col));
+			}
+		}
+
+		return newMatrix;
+	}
+
+	public static Matrix concatDown(Matrix m1, Matrix m2) {
+		if (m1.getColumns() != m2.getColumns()) {
+			throw new InvalidParameterException("Number of columns on m1 (" + m1.getColumns() + ") is different from the number of columns on m2 (" + m2.getColumns() + ")");
+		}
+
+		Matrix newMatrix = m1.duplicate();
+
+		Integer originalNumberOfLines = newMatrix.getLines();
+
+		newMatrix.setNumberOfLines(originalNumberOfLines + m2.getLines());
+
+		for (int c = 1; c <= newMatrix.getColumns(); c++) {
+			for (int m2Line = 1; m2Line <= m2.getLines(); m2Line++) {
+				newMatrix.setValue(originalNumberOfLines + m2Line, c, m2.getValue(m2Line, c));
 			}
 		}
 
--- src/test/java/com/ufmg/operationsresearch/matrix/ConcatMatrixTest.java
+++ src/test/java/com/ufmg/operationsresearch/matrix/ConcatMatrixTest.java
@@ -1,9 +1,10 @@
 package com.ufmg.operationsresearch.matrix;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
-import java.util.Map.Entry;
+import java.security.InvalidParameterException;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,8 @@ public class ConcatMatrixTest {
 	private HashMapMatrixImpl m1x2;
 	private HashMapMatrixImpl m2x1;
 	private HashMapMatrixImpl m2x2;
+	private HashMapMatrixImpl m2x3;
+	private HashMapMatrixImpl m3x2;
 
 	@BeforeEach
 	public void init() {
@@ -23,11 +26,15 @@ public class ConcatMatrixTest {
 		BigDecimal bdArray1x2[][] = { { new BigDecimal("1"), new BigDecimal("2") } };
 		BigDecimal bdArray2x1[][] = { { new BigDecimal("1") }, { new BigDecimal("3") } };
 		BigDecimal bdArray2x2[][] = { { new BigDecimal("1"), new BigDecimal("2") }, { new BigDecimal("3"), new BigDecimal("4") } };
+		BigDecimal bdArray2x3[][] = { { new BigDecimal("1"), new BigDecimal("2"), new BigDecimal("3") }, { new BigDecimal("4"), new BigDecimal("5"), new BigDecimal("6") } };
+		BigDecimal bdArray3x2[][] = { { new BigDecimal("1"), new BigDecimal("2") }, { new BigDecimal("3"), new BigDecimal("4") }, { new BigDecimal("5"), new BigDecimal("6") } };
 
 		this.m1x1 = new HashMapMatrixImpl(bdArray1x1);
 		this.m1x2 = new HashMapMatrixImpl(bdArray1x2);
 		this.m2x1 = new HashMapMatrixImpl(bdArray2x1);
 		this.m2x2 = new HashMapMatrixImpl(bdArray2x2);
+		this.m2x3 = new HashMapMatrixImpl(bdArray2x3);
+		this.m3x2 = new HashMapMatrixImpl(bdArray3x2);
 	}
 
 	@Test
@@ -39,5 +46,56 @@ public class ConcatMatrixTest {
 
 		String expectedMatrixString = "|1 2 1 |\n" + "|3 4 3 |";
 		assertEquals(expectedMatrixString, concatedMatrix.toString());
+	}
+
+	@Test
+	public void testConcatRight2x3With2x2() {
+		HashMapMatrixImpl concatedMatrix = (HashMapMatrixImpl) ConcatMatrix.concatRight(m2x3, m2x2);
+
+		assertEquals(2, concatedMatrix.getLines());
+		assertEquals(5, concatedMatrix.getColumns());
+
+		String expectedMatrixString = "|1 2 3 1 2 |\n" + "|4 5 6 3 4 |";
+		assertEquals(expectedMatrixString, concatedMatrix.toString());
+	}
+
+	@Test
+	public void testConcatRight2x2With1x1RaisesException() {
+		Exception e = assertThrows(InvalidParameterException.class, () -> {
+			ConcatMatrix.concatRight(m2x2, m1x1);
+		});
+
+		assertEquals("Number of lines on m1 (2) is different from the number of lines on m2 (1)", e.getMessage());
+	}
+
+	@Test
+	public void testConcatDown2x2With1x2() {
+		HashMapMatrixImpl concatedMatrix = (HashMapMatrixImpl) ConcatMatrix.concatDown(m2x2, m1x2);
+
+		assertEquals(3, concatedMatrix.getLines());
+		assertEquals(2, concatedMatrix.getColumns());
+
+		String expectedMatrixString = "|1 2 |\n" + "|3 4 |\n" + "|1 2 |";
+		assertEquals(expectedMatrixString, concatedMatrix.toString());
+	}
+
+	@Test
+	public void testConcatDown3x2With2x2() {
+		HashMapMatrixImpl concatedMatrix = (HashMapMatrixImpl) ConcatMatrix.concatDown(m3x2, m2x2);
+
+		assertEquals(5, concatedMatrix.getLines());
+		assertEquals(2, concatedMatrix.getColumns());
+
+		String expectedMatrixString = "|1 2 |\n" + "|3 4 |\n" + "|5 6 |\n" + "|1 2 |\n" + "|3 4 |";
+		assertEquals(expectedMatrixString, concatedMatrix.toString());
+	}
+
+	@Test
+	public void testConcatDown2x2With1x1RaisesException() {
+		Exception e = assertThrows(InvalidParameterException.class, () -> {
+			ConcatMatrix.concatDown(m2x2, m1x1);
+		});
+
+		assertEquals("Number of columns on m1 (2) is different from the number of columns on m2 (1)", e.getMessage());
 	}
 }
